30ade1ca9a2aa40bf48293fc1ca0fe1dffa0a24f
adamd <adamdoyle630@gmail.com>
2022-11-15 14:31:54 -0500

    Why isn't anything working

--- bin/cli.js
+++ bin/cli.js
@@ -1,22 +1,12 @@
-import { roll } from "/lib/roll.js"
+import roll from "lib/roll.js"
+import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2));
 
-let sides = 6
-let dice = 1
-let rolls = 1
+let sides = args.sides || 6
+let dice = args.dice || 2
+let rolls = args.rolls || 1
 
-if (args.sides) {
-    sides = args.sides
-}
-
-if (args.dice) {
-    dice = args.dice
-}
-
-if (args.rolls) {
-    rolls = args.rolls
-}
-
-console.log(roll.roll(sides, dice, rolls))
+console.log(sides)
 console.log("test")
+console.log(JSON.stringify(roll(sides, dice, rolls)))
