05ca2f296917f04e7492c9cd2b9d4452b7a665cc
Adam Doyle <adamdoyle630@gmail.com>
2022-11-15 16:10:16 -0500

    Created roll-dice module

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,11 @@
-import roll from "lib/roll.js"
-import minimist from "minimist"
+#!/usr/bin/env node
+import roll from "../lib/roll.js";
+import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
-let sides = args.sides || 6
-let dice = args.dice || 2
-let rolls = args.rolls || 1
+const sides = args.sides || 6
+const dice = args.dice || 2
+const rolls = args.rolls || 1
 
-console.log(sides)
-console.log("test")
 console.log(JSON.stringify(roll(sides, dice, rolls)))
