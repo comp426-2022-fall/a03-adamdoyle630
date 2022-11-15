9cc8cdb1c52f5af4082eb2310c66e63e7a2c31bb
adamd <adamdoyle630@gmail.com>
2022-11-14 23:11:36 -0500

    module progress

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,22 @@
 import { roll } from "/lib/roll.js"
 
+const args = minimist(process.argv.slice(2));
 
+let sides = 6
+let dice = 1
+let rolls = 1
+
+if (args.sides) {
+    sides = args.sides
+}
+
+if (args.dice) {
+    dice = args.dice
+}
+
+if (args.rolls) {
+    rolls = args.rolls
+}
+
+console.log(roll.roll(sides, dice, rolls))
+console.log("test")
